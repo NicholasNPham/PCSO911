efb2ff3f9bc65b6efb56afead9576c563f9f54e0
NicholasNPham <npham@sao10.com>
2026-03-09 11:04:25 -0400

    finished one function

--- ftp_connection.py
+++ ftp_connection.py
@@ -5,14 +5,36 @@ from ftplib import FTP
 from key import USERNAME, PASSWORD, FTP_LINK
 
 # CONSTANTS
+LOGIN_SUCCESSFUL = "2"
 
 # FUNCTIONS
-def connect_to_ftp(username, password):
-    ftp = FTP(FTP_LINK)
-    print(ftp.login(username, password))
+def connect_to_ftp(username, password, ftp_link):
+    """
+    Connect to FTP server.
+
+    Args:
+        username: FTP account username.
+        password: FTP account password.
+        ftp_link: FTP server address.
+
+    Returns:
+        FTP connection object or None if login fails.
+    """
+    ftp = FTP(ftp_link)
+    status = ftp.login(username, password)
+
+    if status[0] == LOGIN_SUCCESSFUL:
+        print("Login Successful")
+        return ftp
+    else:
+        return None
 
 # MAIN LOOP SETUP
-connection = connect_to_ftp(USERNAME, PASSWORD)
+connection = connect_to_ftp(USERNAME, PASSWORD, FTP_LINK)
+if connection is None:
+    print("Connection Failed")
+
+# MAIN LOOP
 
 
 
