eac8d3f257dca8dd70b4282addaa4f10a741da67
NicholasNPham <npham@sao10.com>
2026-05-19 13:46:11 -0400

    added examples to make it easier to understand the correct constants that needs to be put there

--- key_template.py
+++ key_template.py
@@ -1,23 +1,24 @@
-USERNAME = ""
-PASSWORD = ""
-FTP_TARGET_FOLDER = ""
-FOLDER_IN_TARGET_FOLDER = ""
-FTP_LINK = ""
-ABSOLUTE_PATH = ""
-TEMP_FILES_FOLDER = r""
+# FTP
+USERNAME = "your_ftp_username"
+PASSWORD = "your_ftp_password"
+FTP_TARGET_FOLDER = "your_ftp_target_folder"
+FOLDER_IN_TARGET_FOLDER = "your_folder_in_target_folder"
+FTP_LINK = "ftp.your-server.com"
+ABSOLUTE_PATH = "/absolute/path/on/ftp/server"
+TEMP_FILES_FOLDER = r"C:\path\to\temp_files_folder"
 
 # STAC3
-WEBSITE = ""
-STAC3_USERNAME = ""
-STAC3_PASSWORD = ""
+WEBSITE = "https://your-stac-instance.com"
+STAC3_USERNAME = "your_stac_username"
+STAC3_PASSWORD = "your_stac_password"
 
 # OUTLOOK EMAILS
-EMAIL_TO = ""
-EMAIL_CC = ""
+EMAIL_TO = "recipient@yourdomain.com"
+EMAIL_CC = "cc_recipient@yourdomain.com"
 
-UNC_PATH_TO_H_DRIVE = r""
-TEMP_DIR =  r""
-CHROME_PATH = r''
+UNC_PATH_TO_H_DRIVE = r"\\server\share\path\to\h_drive"
+TEMP_DIR = r"C:\path\to\temp_dir"
+CHROME_PATH = r"C:\path\to\chromedriver.exe"
 
 # REGEX
-UNIVERSAL_CASE_NUMBER_PATTERN = r''
+UNIVERSAL_CASE_NUMBER_PATTERN = r"your-ucn-regex-pattern"
